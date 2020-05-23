4e43fc1789b8096e876b39006d2fc7ed28455553
amirbigg <amirbig44@gmail.com>
2020-05-23 11:10:55 +0430

    add ep3

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../blog.db'
+db = SQLAlchemy(app)
 
 from blog import routes
---
+++ blog/models.py
@@ -0,0 +1,24 @@
+from blog import db
+import datetime
+
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(30), unique=True, nullable=False)
+	email = db.Column(db.String(60), unique=True, nullable=False)
+	password = db.Column(db.String(60), nullable=False)
+	posts = db.relationship('Post', backref='author', lazy=True)
+
+	def __repr__(self):
+		return f'{self.__class__.__name__}({self.id}, {self.username})'
+
+
+class Post(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(120), nullable=False)
+	date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+	content = db.Column(db.Text, nullable=False)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+	def __repr__(self):
+		return f'{self.__class__.__name__}({self.id}, {self.title[:30]}, {self.date})'
