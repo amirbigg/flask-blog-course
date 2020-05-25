4e16636c759b2c2f865faf0f8eb1f59dc4630e74
amirbigg <amirbig44@gmail.com>
2020-05-25 15:21:52 +0430

    add ep5

--- blog/__init__.py
+++ blog/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'f6a6ec1916a64e3294f4bf45bf183f81'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../blog.db'
 db = SQLAlchemy(app)
 
--- blog/routes.py
+++ blog/routes.py
@@ -1,7 +1,18 @@
 from flask import render_template
 from blog import app
+from blog.forms import RegistrationForm, LoginForm
 
 
 @app.route('/')
-def routes():
+def home():
 	return render_template('home.html')
+
+@app.route('/register')
+def register():
+	form = RegistrationForm()
+	return render_template('register.html', form=form)
+
+@app.route('/login')
+def login():
+	form = LoginForm()
+	return render_template('login.html', form=form)
