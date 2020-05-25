88328839eae7e0adb88fd70d3d4e680d303aebf6
amirbigg <amirbig44@gmail.com>
2020-05-25 16:46:51 +0430

    add ep6

--- blog/routes.py
+++ blog/routes.py
@@ -1,15 +1,22 @@
-from flask import render_template
-from blog import app
+from flask import render_template, redirect, url_for
+from blog import app, db, bcrypt
 from blog.forms import RegistrationForm, LoginForm
+from blog.models import User
 
 
 @app.route('/')
 def home():
 	return render_template('home.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
 	form = RegistrationForm()
+	if form.validate_on_submit():
+		hashed_pass = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+		user = User(username=form.username.data, email=form.email.data, password=hashed_pass)
+		db.session.add(user)
+		db.session.commit()
+		return redirect(url_for('home'))
 	return render_template('register.html', form=form)
 
 @app.route('/login')
