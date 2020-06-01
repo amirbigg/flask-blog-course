5faf3cdde184f8b6cad094bc39c7ce15fef4f323
amirbigg <amirbig44@gmail.com>
2020-06-01 23:12:56 +0430

    add ep17

--- blog/routes.py
+++ blog/routes.py
@@ -96,7 +96,23 @@ def delete(post_id):
 	return redirect(url_for('home'))
 
 
-
+@app.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update(post_id):
+	post = Post.query.get_or_404(post_id)
+	if post.author != current_user:
+		abort(403)
+	form = PostForm()
+	if form.validate_on_submit():
+		post.title = form.title.data
+		post.content = form.content.data
+		db.session.commit()
+		flash('post updated', 'info')
+		return redirect(url_for('detail', post_id=post.id))
+	elif request.method == 'GET':
+		form.title.data = post.title
+		form.content.data = post.content
+	return render_template('update.html', form=form)
 
 
 
